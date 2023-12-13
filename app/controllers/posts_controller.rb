99e79888662635e7254a4f8d23dd2cd491e569ab
kenyounot123 <kenlu519@gmail.com>
2023-12-13 18:19:39 -0500

    Update show view for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :user_id)
+    params.require(:post).permit(:title, :body)
   end
 end
