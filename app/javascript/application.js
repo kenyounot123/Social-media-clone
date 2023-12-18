93162859f6df18eae277ada9f730fa2c82c01ce4
kenyounot123 <kenlu519@gmail.com>
2023-12-18 16:52:49 -0500

    Update stim controller to reset form after submit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers/hello_controller.js"
-import * as bootstrap from "bootstrap"
+import "./controllers"
 
+import * as bootstrap from "bootstrap"
