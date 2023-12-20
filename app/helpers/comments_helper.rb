69e696a8136e99d015009dd8a3eb85dc59cfb0dc
kenyounot123 <kenlu519@gmail.com>
2023-12-19 20:47:28 -0500

    Add #find_username helper method

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def find_username(user_id)
+    User.find(user_id).name
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-
   belongs_to :user
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :delete_all
+
   
   scope :ordered, -> { order(id: :desc) }
 end
