a02a8f2702920e6000bab90dfa6ded393d40d357
kenyounot123 <kenlu519@gmail.com>
2024-01-06 11:28:10 -0500

    Write system tests for visiting the dashboard page

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -8,9 +8,23 @@ RSpec.describe 'Posts', type: :system do
   context 'when visiting dashboard page' do 
     it 'shows posts created by users who you follow' do
       sign_in user
+      other_user = create(:user)
+      create(:post, user: other_user)
+      user.follow(other_user)
       visit dashboard_path
+      expect(page.has_content? "#{other_user.name} posted").to be true
     end
+    it 'shows no posts if you follow no one and have not created posts' do 
+      sign_in user
+      visit dashboard_path
+      expect(page.has_no_content?("posted")).to be true
+    end
+
     it 'shows your own created posts' do 
+      sign_in user
+      create(:post, user: user)
+      visit dashboard_path
+      expect(page.has_content? "#{user.name} posted").to be true
     end
   end
 end
