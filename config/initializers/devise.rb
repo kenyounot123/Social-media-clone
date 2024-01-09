ec7dd2f8784e89d1eeb3936335735b5c94a29f2d
kenyounot123 <kenlu519@gmail.com>
2024-01-09 15:41:37 -0500

    Change github oauth env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :github,
-                  ENV["github_client_id"], 
-                  ENV["github_client_secret"],
+                  ENV["GITHUB_CLIENT_ID"], 
+                  ENV["GITHUB_CLIENT_SECRET"],
                   scope: "user:email"
 
   # ==> Warden configuration
