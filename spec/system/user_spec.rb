3a8ef1ba50f2f1221a66fcd116576be4c1521f39
kenyounot123 <kenlu519@gmail.com>
2024-01-09 00:46:51 -0500

    Add new github oauth keys for production

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do 
+  context 'when signing in' do 
+    it 'goes to dashboard path with existing account' do 
+    end
+    it 'goes to edit username page with newly created accounts' do 
+    end
+  end
+  context 'when signing up' do 
+    
+  end
+
+end
